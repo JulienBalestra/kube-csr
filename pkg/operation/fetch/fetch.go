df197ecfd98d7b9ad77f6081ef8c011677457891
Julien Balestra <julien.balestra@gmail.com>
2018-06-09 16:42:53 +0200

    fetch: set annotations to track the activity of fetch (#12)

--- pkg/operation/fetch/fetch.go
+++ pkg/operation/fetch/fetch.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/golang/glog"
 	certificates "k8s.io/api/certificates/v1beta1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/JulienBalestra/kube-csr/pkg/operation/generate"
 	"github.com/JulienBalestra/kube-csr/pkg/utils/kubeclient"
 	"github.com/JulienBalestra/kube-csr/pkg/utils/pemio"
+)
+
+const (
+	// KubeCsrFetchedAnnotationDate is an annotation used to store the timestamp when the certificated has been fetched
+	// This annotation is overridden by the latest fetch
+	KubeCsrFetchedAnnotationDate = "alpha.kube-csr/lastFetched"
+
+	// KubeCsrFetchedAnnotationNb is an annotation used to count the number of fetches of the certificate
+	KubeCsrFetchedAnnotationNb = "alpha.kube-csr/countFetched"
 )
 
 // Config of the Fetch
@@ -43,6 +53,34 @@ func NewFetcher(kubeConfigPath string, conf *Config) (*Fetch, error) {
 	}, nil
 }
 
+func (f *Fetch) updateAnnotations(r *certificates.CertificateSigningRequest) error {
+	now := time.Now().UTC().Format("2006-01-02T15:04:05Z")
+	if r.Annotations == nil {
+		r.Annotations = map[string]string{
+			KubeCsrFetchedAnnotationDate: now,
+			KubeCsrFetchedAnnotationNb:   "1",
+		}
+	} else {
+		r.Annotations[KubeCsrFetchedAnnotationDate] = now
+		nbString := r.Annotations[KubeCsrFetchedAnnotationNb]
+		// if the annotation doesn't exists, nbString is set to "" which is transformed to 0 by Atoi
+		nb, err := strconv.Atoi(nbString)
+		if err != nil {
+			glog.Warningf("Cannot parse the annotation %s: %q: %v", KubeCsrFetchedAnnotationNb, nbString, err)
+		}
+		r.Annotations[KubeCsrFetchedAnnotationNb] = strconv.Itoa(nb + 1)
+		glog.V(2).Infof("csr/%s was already fetched before, incr %q annotation to %s", r.Name, KubeCsrFetchedAnnotationNb, r.Annotations[KubeCsrFetchedAnnotationNb])
+	}
+
+	glog.V(4).Infof("Annotate csr/%s: %s: %s", r.Name, KubeCsrFetchedAnnotationDate, now)
+	r, err := f.kubeClient.GetCertificateClient().CertificateSigningRequests().Update(r)
+	if err != nil {
+		glog.Errorf("Cannot update annotation of csr/%s: %v", r.Name, err)
+		return err
+	}
+	return nil
+}
+
 // Fetch the generated certificate from the CSR
 func (f *Fetch) Fetch(csr *generate.Config) error {
 	glog.V(2).Infof("Start polling for certificate of csr/%s, every %s, timeout after %s", csr.Name, f.conf.PollingInterval.String(), f.conf.PollingTimeout.String())
@@ -65,12 +103,16 @@ func (f *Fetch) Fetch(csr *generate.Config) error {
 
 		case <-tick.C:
 			// TODO as we are waiting the ticker, if the ticker is set to 10s, we start polling after 10s
-			r, err := f.kubeClient.GetCertificateClient().CertificateSigningRequests().Get(csr.Name, v1.GetOptions{})
+			r, err := f.kubeClient.GetCertificateClient().CertificateSigningRequests().Get(csr.Name, metav1.GetOptions{})
 			if err != nil {
 				glog.Errorf("Unexpected error during certificate fetching of csr/%s: %s", csr.Name, err)
 				return err
 			}
 			if r.Status.Certificate != nil {
+				err := f.updateAnnotations(r)
+				if err != nil {
+					return err
+				}
 				glog.V(3).Infof("csr/%s:\n%s", csr.Name, string(r.Status.Certificate))
 				glog.V(2).Infof("Certificate successfully fetched, writing %d chars to %s", len(r.Status.Certificate), f.conf.CertificateABSPath)
 				return pemio.WriteFile(r.Status.Certificate, f.conf.CertificateABSPath, f.conf.CertificatePermission, f.conf.Override)
