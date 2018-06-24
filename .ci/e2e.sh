274d7a527c2f26b178dd87731a12e31541933117
Julien Balestra <julien.balestra@gmail.com>
2018-06-24 09:42:29 -0400

    Introduce pupernetes as e2e testing engine. (#24)
    * e2e: introduce pupernetes
    * travis: refactor the e2e suite to use it

---
+++ .ci/e2e.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+cd $(dirname $0)/..
+
+./kube-csr issue e2e --generate --submit --approve --fetch --kubeconfig-path $HOME/.kube/config
+./kube-csr issue e2e --generate --submit --approve --fetch --override --kubeconfig-path $HOME/.kube/config
+kubectl get secret -o json | jq -re '.items[].data["ca.crt"]' | base64 -d > ca.crt
+openssl verify -CAfile ca.crt kube-csr.certificate
+
+kubectl apply -f examples/etcd.yaml
+timeout 600 ./.ci/etcd.sh
+
+./kube-csr garbage-collect --fetched --grace-period=1h --kubeconfig-path $HOME/.kube/config
+./kube-csr garbage-collect --denied --grace-period=0s --kubeconfig-path $HOME/.kube/config
+./kube-csr garbage-collect --fetched --grace-period=0s --kubeconfig-path $HOME/.kube/config
+./kube-csr garbage-collect --fetched --denied --grace-period=0s --kubeconfig-path $HOME/.kube/config
