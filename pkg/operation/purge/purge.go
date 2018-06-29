55e8e4810236d0bc2f96f6a7a1c55d31a7f20358
Julien Balestra <julien.balestra@gmail.com>
2018-06-29 10:32:18 +0200

    Add a pprof http handler (#31)
    * purge: add a pprof http handler

--- pkg/operation/purge/purge.go
+++ pkg/operation/purge/purge.go
@@ -10,10 +10,12 @@ import (
 	"time"
 
 	"github.com/golang/glog"
+	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	certificates "k8s.io/api/certificates/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/http/pprof"
 
 	"github.com/JulienBalestra/kube-csr/pkg/operation/fetch"
 	"github.com/JulienBalestra/kube-csr/pkg/utils/kubeclient"
@@ -224,16 +226,42 @@ func (p *Purge) GarbageCollect() error {
 	return nil
 }
 
+func (p *Purge) registerAPI() {
+	if p.conf.PrometheusExporterBindAddress != "" {
+		promRouter := mux.NewRouter()
+		promRouter.Path(prometheusExporterPath).Methods("GET").Handler(promhttp.Handler())
+		promServer := &http.Server{
+			Handler:      promRouter,
+			Addr:         p.conf.PrometheusExporterBindAddress,
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+		glog.V(0).Infof("Starting prometheus exporter on %s%s", p.conf.PrometheusExporterBindAddress, prometheusExporterPath)
+		go promServer.ListenAndServe()
+	}
+
+	// Known issue with Mux and the registering of pprof:
+	// https://stackoverflow.com/questions/19591065/profiling-go-web-application-built-with-gorillas-mux-with-net-http-pprof
+	const pprofBind = "127.0.0.1:6060"
+	pprofRouter := mux.NewRouter()
+	pprofRouter.HandleFunc("/debug/pprof/", pprof.Index)
+	pprofRouter.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	pprofRouter.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	pprofRouter.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	pprofRouter.PathPrefix("/debug/pprof/").Handler(http.DefaultServeMux)
+	pprofServer := &http.Server{
+		Handler:      pprofRouter,
+		Addr:         pprofBind,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  60 * time.Second,
+	}
+	glog.V(0).Infof("Starting pprof on %s/debug/pprof", pprofBind)
+	go pprofServer.ListenAndServe()
+}
+
 // GarbageCollectLoop runs the GC on ticker, returns on error or SIGINT/TERM
 func (p *Purge) GarbageCollectLoop() error {
-	if p.conf.PrometheusExporterBindAddress != "" {
-		glog.V(0).Infof("Starting prometheus exporter on %s%s", p.conf.PrometheusExporterBindAddress, prometheusExporterPath)
-		http.Handle(prometheusExporterPath, promhttp.Handler())
-		go func() {
-			err := http.ListenAndServe(p.conf.PrometheusExporterBindAddress, nil)
-			glog.Errorf("Unexpected error while starting prometheus exporter: %v", err)
-		}()
-	}
+	p.registerAPI()
 	tick := time.NewTicker(p.conf.PollingPeriod)
 	defer tick.Stop()
 
