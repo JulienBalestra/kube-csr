a350b9cf7d256c33be84348d6b86526eeb611f29
Julien Balestra <julien.balestra@gmail.com>
2018-08-09 09:21:13 +0200

    Metrics-server is self registered (#50)
    * examples: metrics-server is self registered

--- .ci/e2e.sh
+++ .ci/e2e.sh
@@ -4,9 +4,7 @@ set -exuo pipefail
 
 cd $(dirname $0)/..
 
-REQUEST_HEADER_CLIENT_CA=$(kubectl get cm -n kube-system extension-apiserver-authentication -o json | jq '.data["requestheader-client-ca-file"]' -re | base64 | tr -d '\n')
-sed s/REQUEST_HEADER_CLIENT_CA/"${REQUEST_HEADER_CLIENT_CA}"/g examples/metrics-server.yaml > ./.ci/metrics-server.yaml
-kubectl apply -f ./.ci/metrics-server.yaml
+kubectl apply -f examples/metrics-server.yaml
 kubectl apply -f examples/etcd.yaml
 
 go run examples/issue.go
