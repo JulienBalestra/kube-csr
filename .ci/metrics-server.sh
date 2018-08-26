753e20a8c9cae338746009cc9a4ceb8547ff8744
Julien Balestra <julien.balestra@gmail.com>
2018-08-26 16:18:15 +0200

    ci: bump p8s (#55)
    * [ci] bump p8s
    * [ci] fix metrics-server test

--- .ci/metrics-server.sh
+++ .ci/metrics-server.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+set -exuo pipefail
+
 while true
 do
     kubectl get deploy,rs,po,csr -n kube-system -o wide
-    kubectl top no && kubectl top po --all-namespaces && exit 0
-    echo "===== metrics-server ====="
-    kubectl logs -n kube-system -l k8s-app=metrics-server
-    echo "===== metrics-server ====="
+    kubectl top no && exit 0
+    for po in $(kubectl get po -n kube-system -l k8s-app=metrics-server -o json | jq -re .items[].metadata.name)
+    do
+        echo "===== metrics-server ====="
+        kubectl logs -n kube-system ${po} metrics-server
+        echo "===== metrics-server ====="
+        echo "===== kube-csr-renew ====="
+        kubectl logs -n kube-system ${po} metrics-server
+        echo "===== kube-csr-renew ====="
+    done
     sleep 10
 done
